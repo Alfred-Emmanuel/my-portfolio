519f301391642cfb0651ad129441e6cbe4afbf12
Alfred Emmanuel <ealfred828@gmail.com>
2023-02-19 08:34:37 +0100

    Component folder
    Added my components after creating my component folder

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+function App() {
+    return ( 
+        <h1>My Portfolio</h1>
+     );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function Header() {
+    return ( <h1>The Header</h1> );
+}
+
+export default Header;
